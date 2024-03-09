7ed1cc328ed0208799b516842ee9c7a1c83a1bad
heberjs <heberjsanchez@gmail.com>
2024-03-09 13:36:56 +0100

    WIP isdigram task #335

--- staff/heber-sanchez/isdigram/data.js
+++ staff/heber-sanchez/isdigram/data.js
@@ -1,3 +1,108 @@
-//data
+//data layer
+var data = (function () {
+  //helper
+  function generateId() {
+    return (+parseInt(Math.random() * 10 ** 17).toString()).toString(36);
+  }
 
-var users = localStorage.users ? JSON.parse(localStorage.users) : [];
+  function loadUsers() {
+    return JSON.parse(localStorage.users || "[]");
+  }
+
+  function loadPosts() {
+    return JSON.parse(localStorage.posts || "[]");
+  }
+
+  function saveUsers(users) {
+    localStorage.users = JSON.stringify(users);
+  }
+
+  function savePosts(posts) {
+    localStorage.posts = JSON.stringify(posts);
+  }
+
+  //data
+  function findUser(callback) {
+    var users = loadUsers();
+
+    var user = users.find(callback);
+
+    return user;
+  }
+
+  function insertUser(user) {
+    var users = loadUsers();
+
+    user.id = generateId();
+
+    users.push(user);
+
+    saveUsers(users);
+  }
+
+  function printUsers() {
+    var users = loadUsers();
+    console.table(users);
+  }
+
+  function updateUser(user) {
+    var users = loadUsers();
+    var index = users.findIndex(function (userDb) {
+      return userDb.id === user.id;
+    });
+    if (index > -1) {
+      users.splice(index, 1, user);
+      saveUsers(users);
+    }
+  }
+
+  function getAllUsers() {
+    var users = loadUsers();
+    return users;
+  }
+
+  function insertPost(post) {
+    var posts = loadPosts();
+
+    post.id = generateId();
+
+    posts.push(post);
+
+    savePosts(posts);
+  }
+
+  function getAllPosts() {
+    var posts = loadPosts();
+    return posts;
+  }
+
+  function findPost(callback) {
+    var posts = loadPosts();
+
+    var post = posts.find(callback);
+
+    return post;
+  }
+
+  function deletePost(callback) {
+    var posts = loadPosts();
+
+    var index = posts.findIndex(callback);
+
+    posts.splice(index, 1);
+
+    savePosts(posts);
+  }
+
+  return {
+    findUser: findUser,
+    insertUser: insertUser,
+    printUsers: printUsers,
+    updateUser: updateUser,
+    getAllUsers: getAllUsers,
+    insertPost: insertPost,
+    getAllPosts: getAllPosts,
+    findPost: findPost,
+    deletePost: deletePost,
+  };
+})();
--- staff/heber-sanchez/isdigram/logic.js
+++ staff/heber-sanchez/isdigram/logic.js
@@ -1,40 +1,193 @@
 // business(logic)
+var logic = (function () {
+  //constants
 
-function registerUser(name, birthdate, email, username, password) {
-  var exists = users.some(function (user) {
-    return user.email === email || user.username === username;
-  });
-  if (exists) throw new Error("user already exists");
+  var DATE_REGEX = /^\d{4}-\d{2}-\d{2}$/;
+  var EMAIL_REGEX =
+    /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  var PASSWORD_REGEX = /^(?=.*[0-9])(?=.*[A-Za-z])[A-Za-z0-9]+$/;
+  var URL_REGEX = /^(http|https):\/\//;
 
-  var user = {
-    name: name,
-    birthdate: birthdate,
-    email: email,
-    username: username,
-    password: password,
+  //helpers
+  function validateText(text, explain, checkEmptySpaceInside) {
+    if (typeof text !== "string")
+      throw new Error(explain + " " + text + " is not a string");
+    if (!text.trim().length)
+      throw new Error(explain + " >" + text + "< is empty or blank");
+    if (checkEmptySpaceInside) {
+      if (text.includes(" "))
+        throw new Error(explain + " " + text + " has empty spaces");
+    }
+  }
+  function validateDate(date, explain) {
+    if (!DATE_REGEX.test(date))
+      throw new Error(explain + " " + email + " is not an email");
+  }
+
+  function validateEmail(email, explain) {
+    if (!EMAIL_REGEX.test(email))
+      throw new Error(explain + " " + email + " is not an email");
+  }
+  function validatePassword(password, explain) {
+    if (!PASSWORD_REGEX.test(password))
+      throw new Error(explain + " " + password + " is not acceptable");
+  }
+
+  function validateUrl(url, explain) {
+    if (!URL_REGEX.test(url))
+      throw new Error(explain + " " + url + " is not an url");
+  }
+
+  //logic
+
+  function registerUser(name, birthdate, email, username, password) {
+    validateText(name, "name");
+    validateDate(birthdate, "birthdate");
+    validateEmail(email, "email");
+    validateText(username, "username", true);
+    validatePassword(password, "password");
+
+    var user = data.findUser(function (user) {
+      return user.email === email || user.username === username;
+    });
+
+    if (user) throw new Error("user already exists");
+
+    user = {
+      name: name.trim(),
+      birthdate: birthdate,
+      email: email,
+      username: username,
+      password: password,
+      status: "offline",
+    };
+
+    data.insertUser(user);
+  }
+
+  function loginUser(username, password) {
+    validateText(username, "username", true);
+    validatePassword(password, "password");
+
+    var user = data.findUser(function (user) {
+      return user.username === username && user.password === password;
+    });
+
+    if (!user) throw new Error("wrong credentials");
+
+    user.status = "online";
+
+    data.updateUser(user);
+
+    sessionStorage.userId = user.id;
+  }
+
+  function retrieveUser() {
+    var user = data.findUser(function (user) {
+      return user.id === sessionStorage.userId;
+    });
+
+    if (!user) throw new Error("user not found");
+
+    return user;
+  }
+
+  function logoutUser() {
+    var user = data.findUser(function (user) {
+      return user.id === sessionStorage.userId;
+    });
+    if (!user) throw new Error("wrong credentials");
+    user.status = "offline";
+    data.updateUser(user);
+    delete sessionStorage.userId;
+  }
+
+  function getLoggedUserId() {
+    return sessionStorage.userId;
+  }
+
+  function isUserLoggedIn() {
+    return !!sessionStorage.userId;
+  }
+
+  function retrieveUsers() {
+    var users = data.getAllUsers();
+
+    var index = users.findIndex(function (user) {
+      return user.id == sessionStorage.userid;
+    });
+    users.splice(index, 1);
+
+    users.forEach(function (user) {
+      delete user.name;
+      delete user.email;
+      delete user.password;
+      delete user.birthdate;
+    });
+
+    users
+      .sort(function (a, b) {
+        return a.username < b.username ? -1 : 1;
+      })
+      .sort(function (a, b) {
+        return a.status > b.status ? -1 : 1;
+      });
+
+    return users;
+  }
+
+  function createPost(image, text) {
+    validateUrl(image, "image");
+    if (text) validateText(text, "text");
+
+    var post = {
+      author: sessionStorage.userId,
+      image: image,
+      text: text,
+      date: new Date().toLocaleDateString("en-CA"),
+    };
+
+    data.insertPost(post);
+  }
+
+  function retrievePosts() {
+    var posts = data.getAllPosts();
+
+    posts.forEach(function (post) {
+      var user = data.findUser(function (user) {
+        return user.id === post.author;
+      });
+      post.author = { id: user.id, username: user.username };
+    });
+
+    return posts.reverse();
+  }
+
+  function removePost(postId) {
+    validateText(postId, "postId", true);
+
+    var post = data.findPost(function (post) {
+      return post.id === postId;
+    });
+    if (!post) throw new Error("post not found");
+    if (post.author !== sessionStorage.userId)
+      throw new Error("post does not belong to user");
+
+    data.deletePost(function (post) {
+      return post.id === postId;
+    });
+  }
+
+  return {
+    registerUser: registerUser,
+    loginUser: loginUser,
+    retrieveUser: retrieveUser,
+    logoutUser: logoutUser,
+    getLoggedUserId: getLoggedUserId,
+    isUserLoggedIn: isUserLoggedIn,
+    retrieveUsers: retrieveUsers,
+    createPost: createPost,
+    retrievePosts: retrievePosts,
+    removePost: removePost,
   };
-
-  users.push(user);
-
-  localStorage.users = JSON.stringify(users);
-}
-
-function loginUser(username, password) {
-  var match = users.some(function (user) {
-    return user.username === username && user.password === password;
-  });
-
-  if (!match) throw new Error("wrong credentials");
-
-  sessionStorage.username = username;
-}
-
-function retrieveUser(username) {
-  var user = users.find(function (user) {
-    return user.username === username;
-  });
-
-  if (!user) throw new Error("user not found");
-
-  return user;
-}
+})();
