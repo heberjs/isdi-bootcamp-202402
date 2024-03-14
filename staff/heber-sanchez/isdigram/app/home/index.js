7e9a3f9a84d059ba71806eb9b3eaea2391372002
heberjs <heberjsanchez@gmail.com>
2024-03-14 14:37:58 +0100

    add O.O.P on app WIP #335

---
+++ staff/heber-sanchez/isdigram/app/home/index.js
@@ -0,0 +1,33 @@
+// presentation
+
+(function () {
+  if (!logic.isUserLoggedIn()) {
+    location.href = "../login";
+
+    return;
+  }
+
+  var home = new Component("main");
+
+  home.assambleTo(document.body);
+
+  try {
+    var user = logic.retrieveUser();
+
+    var title = new Component("h1");
+
+    title.setText("Hello, " + user.name + "!");
+
+    home.add(title);
+  } catch (error) {
+    showFeedback(error);
+  }
+
+  var menu = new Menu();
+
+  home.add(menu);
+
+  var posts = new Posts();
+
+  home.add(posts);
+})();
